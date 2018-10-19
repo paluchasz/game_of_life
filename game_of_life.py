9ea4c0799a7382db8b12f8bc1938db12a1b6d9cf
paluchasz <paluchasz@googlemail.com>
2018-10-19 20:35:31 +0100

    Half the project done perhaps
    Now putting things together for cell death and drawing the board in a method called playTheGame(self). Since with only cell death there is
    only one iteration I show the initial and final boards. I checked quite a few test cases and seems to work correctly.
    
    Now next time I need to think about adding cell creation. I guess what I could do is add extra layer of zeros around the initial board and
    then can just to the same approach as with cell death, however I will not always need to create an extra layer with each iteration so what
    else can I do? Could look if there is a cluster of 1s (ie 3 1s in row) on the edges and only then creat extra but this seems kind of
    involved so I might try the initial approach first.

--- game_of_life.py
+++ game_of_life.py
@@ -1,11 +1,52 @@
 import copy
+from PIL import Image, ImageDraw
 
 class PlayGame:
     def __init__(self, board):
         self.board = board
 
     def drawBoard(self):
-        return self.board
+        horiz_step_count = len(self.board[0]) # number of columns
+        vert_step_count = len(self.board) # number of rows
+        width = 600
+        height = 600
+
+        # Create an image (white square) of size widthxheight:
+        image = Image.new(mode='L', size=(width, height), color=255)
+
+        # First draw the grid by drawing the required number of lines:
+        draw = ImageDraw.Draw(image)
+
+        # Need to set the the seperation of the lines:
+        horiz_step_size = int(image.width / horiz_step_count)
+        vert_step_size = int(image.height / vert_step_count)
+
+        y_start = 0
+        y_end = image.height
+
+        # line =((a,b),(c,d)) creates a line from (a,b) to (c,d)
+        for x in range(0, image.width, horiz_step_size):
+            line = ((x, y_start), (x, y_end))
+            draw.line(line, fill=128)
+
+        x_start = 0
+        x_end = image.width
+
+        for y in range(0, image.height, vert_step_size):
+            line = ((x_start, y), (x_end, y))
+            draw.line(line, fill=128)
+
+        # Now draw the cells:
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+
+                if self.board[i][j] == 1:
+                    draw.ellipse((j * vert_step_size, i * horiz_step_size, (j+1) * vert_step_size, (i+1) * horiz_step_size), fill = 'red', outline ='red')
+
+        del draw
+
+        image.show()
+
 
     def interactions(self):
         # Creating a copy of the board so that while I update the board this doesnt
@@ -222,11 +263,18 @@ class PlayGame:
 
         return self.board
 
+#playgame method which will take number of iterations and call other methods
+
+    def playTheGame(self):
+        self.drawBoard()
+        self.interactions()
+        self.drawBoard()
+
 
 if __name__ == '__main__':
     # print("  _")
     # print("|","_","|")
     # print("|","_","|")
-    board = [[1,0,1,0],[0,1,1,1],[0,1,1,0],[1,0,0,1]]
+    board = [[1,0,1,0,0,1,0],[0,1,1,1,0,1,1],[0,1,1,0,1,0,1],[1,0,0,1,0,0,1],[1,1,0,0,1,0,0],[1,1,0,0,1,1,0],[1,1,1,0,0,1,0]]
     game1 = PlayGame(board)
-    game1.interactions()
+    game1.playTheGame()
