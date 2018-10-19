bd3bfc88d434df28cbd9f65c6201b78abb1bc3d4
paluchasz <paluchasz@googlemail.com>
2018-10-19 20:19:56 +0100

    Finally sorted out cell death
    I added all the differnt cases for the interactions method for cell death. Initially I thought that I did not have to treat the first row
    and first column seperately as it was not running into errors. Then after doing some test cases (with only 1 cell in top row and some in
    the bottom row I was getting a non zero neighbour count for the first cell which was strange!!) I realised that this is because if we have
    i=0 and we look at board[i-1] we have board[-1] and that is an element in the last row! So I treated all cases seperately, ie all the
    corners, first and last rows and columns and the rest (middle).

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,4 @@
+import copy
 
 class PlayGame:
     def __init__(self, board):
@@ -7,45 +8,220 @@ class PlayGame:
         return self.board
 
     def interactions(self):
+        # Creating a copy of the board so that while I update the board this doesnt
+        # affect cells which should/shouldn't be killed but aren't/are
         temp_board = copy.deepcopy(self.board)
 
-        for i in range(len(temp_board)):
-            for j in range(len(temp_board)):
+# For the middle of the board (start index at 1 and end at len-2):
+
+        for i in range(1,len(temp_board)-1):
+            for j in range(1,len(temp_board[i])-1):
+
+# If there is a cell present (==1) then check and keep count of neighbouring cells
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j-1] == 1:
+                        count += 1
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i-1][j+1] == 1:
+                        count += 1
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+                    if temp_board[i+1][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+                    if temp_board[i+1][j+1] == 1:
+                        count += 1
+                    #print("count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+                        #print("self: ", self.board)
+                        #print("temp:", temp_board)
+
+# Case for the first row:
+
+        for i in range(0,1):
+            for j in range(1,len(temp_board[i])-1):
 
                 if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
 
-                    print("i:",i,'j:',j)
-                    if self.cellDeath(i,j) == True:
-    #IMPORTANT, had an error here because when calling the method I was not using self but just cellDeath
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+                    if temp_board[i+1][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+                    if temp_board[i+1][j+1] == 1:
+                        count += 1
+                    #print("first row count:", count)
+                    if count < 2 or count > 3:
                         self.board[i][j] = 0
-                        print("self: ", self.board)
-                        print("temp:", temp_board)
+
+
+# Case for the first column:
+        for i in range(1,len(temp_board)-1):
+            for j in range(0,1):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i-1][j+1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+                    if temp_board[i+1][j+1] == 1:
+                        count += 1
+                    #print("first column count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for the last row:
+        for i in range(len(temp_board)-1, len(temp_board)):
+            for j in range(1,len(temp_board[i])-1):
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j-1] == 1:
+                        count += 1
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i-1][j+1] == 1:
+                        count += 1
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+                    #print("last row count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for the last column
+        for i in range(1,len(temp_board)-1):
+            for j in range(len(temp_board[i])-1, len(temp_board[i])):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j-1] == 1:
+                        count += 1
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+
+                    #print("last column count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for [0,0]
+
+        for i in range(0, 1):
+            for j in range(0, 1):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+                    if temp_board[i+1][j+1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+
+                    #print("[0,0] count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for [0,len-1]
+
+        for i in range(0, 1):
+            for j in range(len(temp_board[i])-1, len(temp_board[i])):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j-1] == 1:
+                        count += 1
+                    if temp_board[i+1][j] == 1:
+                        count += 1
+
+                    #print("[0,len-1] count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for [len-1,0]
+
+        for i in range(len(temp_board)-1, len(temp_board)):
+            for j in range(0,1):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i-1][j+1] == 1:
+                        count += 1
+                    if temp_board[i][j+1] == 1:
+                        count += 1
+
+                    #print("[len-1,0] count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
+
+
+#Case for [len-1,len-1]
+
+        for i in range(len(temp_board)-1, len(temp_board)):
+            for j in range(len(temp_board[i])-1, len(temp_board[i])):
+
+                if temp_board[i][j] == 1:
+                    count = 0
+                    #print("i:",i,'j:',j)
+
+                    if temp_board[i-1][j-1] == 1:
+                        count += 1
+                    if temp_board[i-1][j] == 1:
+                        count += 1
+                    if temp_board[i][j-1] == 1:
+                        count += 1
+
+                    #print("[len-1,len-1] count:", count)
+                    if count < 2 or count > 3:
+                        self.board[i][j] = 0
 
         return self.board
 
-    def cellDeath(self, i, j):
-        count = 0
-
-        if self.board[i-1][j-1] == 1:
-            count += 1
-        if self.board[i-1][j] == 1:
-            count += 1
-        if self.board[i-1][j+1] == 1:
-            count += 1
-        if self.board[i][j-1] == 1:
-            count += 1
-        if self.board[i][j+1] == 1:
-            count += 1
-        if self.board[i+1][j-1] == 1:
-            count += 1
-        if self.board[i+1][j] == 1:
-            count += 1
-        if self.board[i+1][j+1] == 1:
-            count += 1
-        print("count:", count)
-        if count < 2 or count > 3:
-            return True
-        return False
 
 if __name__ == '__main__':
     # print("  _")
